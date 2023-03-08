b709c35a9e1e929550bb5d4151d5d388294ffb14
dementedchickens <123401219+dementedchickens@users.noreply.github.com>
2023-03-07 18:51:41 -0600

    yup
    ghjjuv

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-index.js
+
+var yes = document.getElementById("yes");
+
+yes.addEventListener("click", function() {
+var yes= document.getElementById("yes");
+  if (yes.textContent === "click me:)") {
+    yes.textContent = "You clicked the paragraph!";
+  } else {
+    yes.textContent = "You already clicked the paragraph!";
+  }
+});
